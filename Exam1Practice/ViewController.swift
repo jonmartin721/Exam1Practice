26890761eb63827e1b3ff766d0135f997a656cde
Jonathan Martin <jonmartin721@gmail.com>
2017-10-02 21:40:05 -0500

    Initial Commit

---
+++ Exam1Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exam1Practice
+//
+//  Created by Jonathan on 10/2/17.
+//  Copyright © 2017 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
