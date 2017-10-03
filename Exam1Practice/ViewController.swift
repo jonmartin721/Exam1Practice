98daa9e161674d08f4cae743043687d05d3c5f04
Jonathan Martin <jonmartin721@gmail.com>
2017-10-02 22:51:13 -0500

    Beginning work on time labels.

--- Exam1Practice/ViewController.swift
+++ Exam1Practice/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //outlets for date and time
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+    //calculating the time and date
+    let date = NSDate()
+    let calendar = NSCalendar.current
+    let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate: date)
+    let hour = components.hour
+    let minutes = components.minute
+    
+    var currentDateTime = Date()
+    let dateFormatter = DateFormatter()
+    dateFormatter.timeStyle = .medium
+    dateLabel.text = "\(dateFormatter.string(from:currentDateTime))"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
